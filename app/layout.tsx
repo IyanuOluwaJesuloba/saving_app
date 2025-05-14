5266150682093162da8250423e74687751dd34dd
IyanuOluwaJesuloba <jesulobaowoseni1@gmail.com>
2025-05-14 17:32:54 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import './globals.css'
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
